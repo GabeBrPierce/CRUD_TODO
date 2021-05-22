62a7bf120c578d8f54007ed2f31fd2a9514aff4b
GabeBrPierce <82479780+GabeBrPierce@users.noreply.github.com>
2021-05-22 01:21:01 -0600

    Added edit and delete
    Added the ability to edit and delete the values. I guess this is pretty much finished. I'll focus on it more to review details.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ class note {
         this.id = id;
     }
     display() {
-        return '<div class="row" ><input type="checkbox" class="checkbox col-1" id = "' + this.id + '"' + (this.checked ? 'checked' : '') + '><div class="col-1">' + this.text + "</div></div>";
+        return '<div class="row" id = "' + this.id + '"><input type="checkbox" class="checkbox col-1"' + (this.checked ? 'checked' : '') + '><div class="col-4 to-do-label">' + this.text + "</div><div class='col-2'><button class='primary edit'>edit</button></div><div class='col-2'><button class='primary delete'>delete</button></div></div>";
     }
 }
 // test notes
@@ -70,19 +70,47 @@ function addListeners() {
     // and searches for a note object with the same id and sets it 
     // value.
     $(".checkbox").change(function () {
-        var selected_id = this.id;
+        var selected_id = $(this).parent().attr('id');
         var selected_value = this.checked;
         todo_list.forEach(element => {
             if (element.id == selected_id) {
                 element.checked = selected_value;
-                
             }
         });
         update();
     });
-    $("#newNoteButton").click(function (e) { 
+    $("#newNoteButton").click(function (e) {
         counter++;
-        todo_list.push(new note($('#newNoteText').val(),counter));
+        todo_list.push(new note($('#newNoteText').val(), counter));
+        update();
+    });
+    $(".edit").click(function () {
+        var selected_parent = $(this).parent().parent();
+        var old_label = $(selected_parent).find(".to-do-label").text();
+        var old_id = $(selected_parent).attr('id');
+        alert(old_label);
+        selected_parent.text("");
+        selected_parent.html('<div class="row" id = "' + old_id + '"><div class="col-4"></div><input type = "text" class="to-do-label" value ="'+ old_label + '">' + "</div><div class='col-2'><button class='primary save'>save</button></div></div>");
+        removeListeners();
+        addListeners();
+    });
+    $(".save").click(function () {
+        var selected_parent = $(this).parent().parent();
+        var new_label = selected_parent.find('.to-do-label').val();
+        todo_list.forEach(element => {
+            if (element.id == selected_parent.attr('id')){
+                element.text = new_label;
+            }
+        });
+        update();
+    });
+    $(".delete").click(function () {
+        var selected_id = $(this).parent().parent().attr('id');
+        
+        var temp_todo_list = todo_list.filter(function (value, index, arr) {
+            return value.id != selected_id;
+        });
+        todo_list = temp_todo_list;
         update();
     });
 }
@@ -90,6 +118,9 @@ function removeListeners() {
     $("#options").off();
     $(".checkbox").off();
     $("#newNoteButton").off();
+    $(".edit").off();
+    $(".delete").off();
+    $(".save").off();
 }
 // start up method
 $(function () {
